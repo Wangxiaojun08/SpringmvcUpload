c6a5ac3dc59e4e547753036e820783f52124f526
Wangxiaojun <bluecu@foxmail.com>
2017-05-18 14:13:26 +0800

    update Test

--- src/main/webapp/scripts/ThreadLocal.java
+++
@@ -1,9 +0,0 @@
-private static ThreadLocal<Connection> connectionHolder = new ThreadLocal<Connection>() {
-	public Connection initialValue() {
-		return DriverManager.getConnection(DB_URL);
-	}
-};
-
-public static Connection getConnection() {
-    return connectionHolder.get();
-}
