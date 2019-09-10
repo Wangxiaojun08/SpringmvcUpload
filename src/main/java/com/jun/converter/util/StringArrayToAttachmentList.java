ece083bd60994c9e03b41b66e7e2ed053ec45ace
chenguoping <chenguoping@100.me>
2019-09-10 14:36:41 +0800

    优化if

--- src/main/java/com/jun/converter/util/StringArrayToAttachmentList.java
+++ src/main/java/com/jun/converter/util/StringArrayToAttachmentList.java
@@ -9,15 +9,17 @@ import java.util.List;
 public class StringArrayToAttachmentList implements Converter<String[], List<Attachment>> {
 
     public List<Attachment> convert(String[] source) {
-        if (source == null)
+        if (source == null) {
             return null;
+        }
         List<Attachment> attachs = new ArrayList<Attachment>(source.length);
         Attachment attach = null;
         for (String attachStr : source) {
             // 这里假设我们的Attachment是以“name,requestUrl,size”的形式拼接的。
             String[] attachInfos = attachStr.split(",");
-            if (attachInfos.length != 3)// 当按逗号分隔的数组长度不为3时就抛一个异常，说明非法操作了。
+            if (attachInfos.length != 3) {
                 throw new RuntimeException();
+            }
             String name = attachInfos[0];
             String requestUrl = attachInfos[1];
             int size;
